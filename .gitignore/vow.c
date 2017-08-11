a3a8be9b29ac9dd9808cdb95870de45a2ec378fa
Ragavi96 <30549326+Ragavi96@users.noreply.github.com>
2017-08-11 17:02:16 +0530

    Update and rename .gitignore to vow.c

---
+++ .gitignore/vow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("enter the charcter");
+scanf("%d",&n);
+if(n==a || n==e|| n==i || n==o || n==u)
+{
+printf(vowels");
+}
+else
+{
+printf("cosonants");
+}
+}
